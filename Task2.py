d763c9c85d1aa0c25450679539573df18bd8e623
Alex Kizilov <as_k@list.ru>
2022-12-14 13:29:17 +0400

    Task2 finished

---
+++ Task2.py
@@ -0,0 +1,10 @@
+﻿# Напишите программу для проверки истинности утверждения 
+# ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+letters = ['X','Y','Z']
+xyz = []
+for i in range(3):
+    xyz.append(input(f'Введите {letters[i]}: '))
+
+if not(xyz[0] or xyz[1] or xyz[2]) == (not(xyz[0]) and not(xyz[1]) and not(xyz[1])):
+    print('Утверждение истинно')
